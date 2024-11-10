368a5ac7b28c0fc7182b9dee92a9501f56722717
Shamilsharaf <165153833+Shamilsharaf@users.noreply.github.com>
2024-11-10 16:30:58 +0530

    Implementing search results part 1

--- src/js/model.js
+++ src/js/model.js
@@ -4,14 +4,16 @@ import { getJSON } from './helpers.js';
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
-
 export const loadRecipe = async function (id) {
   try {
     const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
-
     state.recipe = {
       id: recipe.id,
       title: recipe.title,
@@ -30,3 +32,25 @@ export const loadRecipe = async function (id) {
     throw err;
   }
 };
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+    // console.log(state.search.results);
+  } catch (err) {
+    console.err(`$(err)💥💥💥💥`);
+    throw err;
+  }
+};
+loadSearchResults('pizza');
