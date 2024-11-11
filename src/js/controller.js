59da9b1500710e217bc1d18065a27f37cedcf294
Shamilsharaf <165153833+Shamilsharaf@users.noreply.github.com>
2024-11-11 15:45:16 +0530

    Implementing Pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import PaginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -40,14 +41,29 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    resultsView.render(model.state.search.results);
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage(4));
+
+    //  Render initial pagination buttons
+    PaginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
 
+const controlPagination = function (goToPage) {
+  // 3) Render NEW Results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  //  2) Render NEW pagination buttons
+  PaginationView.render(model.state.search);
+
+  console.log(goToPage);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  PaginationView.addHandlerClick(controlPagination);
 };
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -1,5 +1,5 @@
 import { async } from 'regenerator-runtime';
-import { API_URL } from './config.js';
+import { API_URL, RES_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
 
 export const state = {
@@ -7,6 +7,8 @@ export const state = {
   search: {
     query: '',
     results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 export const loadRecipe = async function (id) {
@@ -53,4 +55,20 @@ export const loadSearchResults = async function (query) {
     throw err;
   }
 };
-loadSearchResults('pizza');
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page; // Update the current page in state
+
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+
+  return state.search.results.slice(start, end);
+};
+
+// export const getSearchResultsPage = function (page = state.search.page) {
+//   model.state.search.page = page;
+//   const start = (page - 1) * model.state.search.resultsPerPage; //0;
+//   const end = page * model.state.search.resultsPerPage; //9;
+
+//   return model.state.search.results.slice(start, end);
+// };
